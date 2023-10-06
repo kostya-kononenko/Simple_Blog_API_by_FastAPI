38df5112b69af802cd9d017696cccf5f1b34f44f
konstantin <kkononenko3@gmail.com>
2023-10-06 15:26:48 +0300

    created functionality for get all posts

--- blog/routers.py
+++ blog/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from . import crud
+from .import crud
 from .schemas import PostBase
 from sqlalchemy.orm import Session
 from database.database import get_db
@@ -14,3 +14,8 @@ router = APIRouter(
 @router.post('')
 def create(request: PostBase, db: Session = Depends(get_db)):
     return crud.create(db, request)
+
+
+@router.get('/all')
+def posts(db: Session = Depends(get_db)):
+    return crud.get_all(db)
